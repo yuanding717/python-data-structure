4999e47d82326988f499bd19dbf3cbca29eae9b7
Yuan Ding <yuanding@Yuans-MacBook-Air.local>
2017-07-20 22:57:05 -0400

    add bst

---
+++ bst.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def insert(self, new_val):
+        pass
+
+    def search(self, find_val):
+        return False
